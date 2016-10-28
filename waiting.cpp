06b79e521cf18e5787ab4c5106c3141de3d08727
Jurri Wang <bh.hg2011@qq.com>
2016-10-28 08:40:14 -0700

    clock()

---
+++ waiting.cpp
@@ -0,0 +1,23 @@
+// waiting.cpp -- using clock() in a time-delay loop
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    using namespace std;
+    cout << "Enter the delay time, in seconds: ";
+    float secs;
+    cin >> secs;
+    clock_t delay = secs * CLOCKS_PER_SEC; // convert to clock ticks
+    cout << "clocks per second: " << CLOCKS_PER_SEC << endl;
+    cout << "starting\a\n";
+    clock_t start = clock();
+    while (clock() - start < delay ) // wait until time elapses
+    ; // note the semicolon
+    cout << "done \a\n";
+    return 0;
+}
