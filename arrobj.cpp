d69a707a52b00c2918cffdd92961972ba6a39c51
Jurri Wang <bh.hg2011@qq.com>
2016-10-30 16:36:28 -0700

    Create arrobj.cpp

---
+++ arrobj.cpp
@@ -0,0 +1,42 @@
+//arrobj.cpp -- functions with array objects (C++11)
+#include <iostream>
+#include <array>
+#include <string>
+// constant data
+using namespace std;
+const int Seasons = 4;
+const array<string, Seasons> Snames = {"Spring", "Summer", "Fall", "Winter"};
+
+// function to modify array object
+void fill( array<double, Seasons> * pa);
+// function that uses array object without modifying it
+void show( array<double, Seasons> da);
+
+int main()
+{
+	array<double, Seasons> expenses;
+	fill(&expenses);
+	show(expenses);
+	return 0;
+}
+void fill(array<double, Seasons> * pa)
+{
+	//using namespace std;
+	for (int i = 0; i < Seasons; i++)
+	{
+		cout << "Enter " << Snames[i] << " expenses: ";
+		cin >> (*pa)[i];
+	}
+}
+void show(array<double, Seasons> da)
+{
+	//using namespace std;
+	double total = 0.0;
+	cout << "\nEXPENSES\n";
+	for (int i = 0; i < Seasons; i++)
+	{
+	cout << Snames[i] << ": $" << da[i] << endl;
+	total += da[i];
+	}
+	cout << "Total Expenses: $" << total << endl;
+}
